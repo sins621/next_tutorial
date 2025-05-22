f89bc13718afc54a1e3b1c1732007ea6f1dd251b
sins621 <sinsmailza@gmail.com>
2025-05-22 20:29:12 +0200

    hide images behind sign in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,34 @@
 import { db } from "@/server/db";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
+
+  return (
+    <div className="flex flex-wrap gap-4">
+      {[...images, ...images, ...images].map((image, index) => (
+        <div key={index} className="flex w-48 flex-col">
+          <img src={image.url} />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image, index) => (
-          <div key={index} className="flex w-48 flex-col">
-            <img src={image.url} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-2xl text-center">Please Sign in Above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
       Hello (Gallery in Progress)
     </main>
   );
