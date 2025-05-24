70b91c376d8f1f3f77b949d8b1257cb2eb35055e
sins621 <sinsmailza@gmail.com>
2025-05-24 13:41:52 +0200

    fix modal rendering

--- src/app/photo/[id]/page.tsx
+++ src/app/photo/[id]/page.tsx
@@ -1,8 +1,8 @@
-export default async function PhotoModal({
+export default function PhotoModal({
   params,
 }: {
   params: { id: string };
 }) {
-  const { id: photoId } = await params;
+  const { id: photoId } = params;
   return <div>{photoId}</div>;
 }
