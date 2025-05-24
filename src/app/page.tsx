8a1bfb0b3396427d0d0e5a144511e9d8b4de82e9
bradly <bradly@warpdeveloment.com>
2025-05-24 16:13:27 +0200

    remove placeholder text and add padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export const dynamic = "force-dynamic";
 async function Images() {
   const images = await getImages();
   return (
-    <div className="flex flex-wrap justify-center gap-4">
+    <div className="flex flex-wrap justify-center gap-4 p-4">
       {images.map((image) => (
         <div key={image.id} className="flex h-48 w-48 flex-col">
           <Link href={`/photo/${image.id}`}>
@@ -38,7 +38,6 @@ export default async function HomePage() {
       <SignedIn>
         <Images />
       </SignedIn>
-      Hello (Gallery in Progress)
     </main>
   );
 }
